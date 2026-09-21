269f71697ed2541d0d090dc4b78a3d934676ce5f
JJ0305 <jorgerodriguezlamadrid@gmail.com>
2026-09-21 18:53:12 -0500

    Subiendo el proyecto a la nube

---
+++ src/main/java/com/example/tareaCalificada07/TareaCalificada07Application.java
@@ -0,0 +1,13 @@
+package com.example.tareaCalificada07;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TareaCalificada07Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TareaCalificada07Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tareaCalificada07/TareaCalificada07ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tareaCalificada07;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TareaCalificada07ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
